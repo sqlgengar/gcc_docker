d7f2ce364b6cd0da65ba5264c083b5720770bb55
Ale <ivale008@gmail.com>
2022-09-29 01:37:06 -0300

    add gitignore

--- source/main.c
+++ source/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#define DIM 9
 
 int main(){
 
-    int i;
-    int vecl[DIM] = { 11, 22, 33, 44, 55, 66, 77, 88 , 99 };
+    int first_value = 0,
+	second_value = 0;
 
-    printf( "elemento 4 del vector %d \n", *(&vecl[DIM] + 3) );
 
-    printf( "script done \n" );
+    
+
+
     return 0;
 
 }
