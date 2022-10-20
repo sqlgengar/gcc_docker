a0a70a9a5274f1e31e641b2092e9b8a62e383023
alethinkion <alejandrom@thinkion.com.ar>
2022-10-19 21:52:09 -0300

    add m6p5

--- source/main.c
+++ source/main.c
@@ -1,87 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define SIZE 3
 
-void insert_asc( int[] );
-//void insert_desc( int[] );
-//void insert_pos( int[] );
-void print_array( int[] );
+void show_non_repeat( int[] );
 
 int main(){
 
-	int 	array[SIZE],
-		value_option = 0;
+	int array[SIZE];
 
-	printf( "Program for manipule vectors, please enter a option: \n" );
-	
-
-	while( value_option != 4 ){
+	for( int i = 0; i < SIZE; i++ ){
 		
-		printf( "1) Insert value ascendent \n" );
-		printf( "2) Insert value descendent \n" );
-		printf( "3) Insert value in particular position \n" );
-		printf( "4) Exit program \n" );
-
-		scanf( " %d", &value_option );
-		fflush( stdin );
-
-		switch( value_option ){
-
-			case 1:
-					insert_asc( array );
-					break;
-			case 2:
-					//insert_desc( array );
-					break;
-			case 3:
-					//insert_pos( array );
-					break;
-			case 4:
-					exit( EXIT_SUCCESS );//end program by user option
-					break;
-			default:
-					printf( "Value invalid for option \n" );
-					printf( "Please enter a value between 1-4: \n" );
-					break;
-		}
+		
 
 	}
-
-	printf( "Script done \n" );
-	return 0;
-
-}
-
-
-void insert_asc( int array[SIZE] ){
-
-	int temp;
-
-	printf( "Enter new value for the array \n" );
-	scanf( " %d", &temp );
-	fflush( stdin );
 	
-	for( int i = 0; i < SIZE; i++ ){
-
-		array[i+1] = array[i];
-
-	}
-
-	array[0] = temp;
-	print_array( array );
 
 }
-
-
-
-void print_array( int array[SIZE] ){
-
-	for( int i = 0; i < SIZE; i++ ){
-
-		printf( "array[%d] -> %d \n", i, array[i] );
-
-	}
-
-}
-
-
