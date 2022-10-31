a0430a5af2e5b2cd789de36b4a39c58c54e23405
Ale <ivale008@gmail.com>
2022-10-31 11:25:07 -0300

    update parcial2

--- source/main.c
+++ source/main.c
@@ -1,153 +1,103 @@
 #include <stdio.h>
-#define MAX_GRADES 2 //max value for amount grades by student
-#define MAX_FILES  2 //mas value for amount of input studens
-
-typedef enum{ 
-
-	DESAPROBADO = 0,
-	APROBADO_PRACIAL = 1, 
-	APROBADO = 3 
-
-} Status;
+#define AMOUNT_YEARS 		2 //amount of years that program can load
+#define AMOUNT_MOUNTHS 		2 //amount of mounths that program can load
+#define AMOUNT_DAYS 		2 //amount of days that program can load, without logic of validate
 
 typedef struct{
 
-	int file;
-	int all_grades[MAX_GRADES];
-	Status final_grade;
-	float average;
+	int presents;
+	int missings;
 
-} Students;
+} Days;
 
-Students load_student();
-float calc_avg( Students );
-float max_avg( Students[] );
-void print_results( Students[], float );
+void load_days( Days [AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS] );
+void avg_presents( Days [AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS] );
+void max_presents( Days [AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS] );
 
 int main(){
 
-	Students all_students[MAX_FILES];
-	float max_average;
-
-	for( int i = 0; i < MAX_FILES; i++){
-
-		all_students[i] = load_student();
-
-	}
-
-	for( int j = 0; j < MAX_FILES; j++ ){
-
-		all_students[j].average = calc_avg( all_students[j] );
-
-	}
-
-	max_average = max_avg( all_students );
-
-	print_results( all_students, max_average );
-
-	printf( "script done \n" );
+	Days date[AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS];
 	
+	load_days( date );
+	avg_presents( date );
+	max_presents( data );
+
+	printf( "\n script done \n" );
+
 	return 0;
 
 }
 
-Students load_student(){
+void load_days( Days new_day[AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS] ){
 
-	Students new_student;
+	int index_day;
 
-	printf( "Enter a file number for the student: \n" );
-	scanf( " %d", &new_student.file );
+	for( int y = 0; y < AMOUNT_YEARS; y++ ){
+
+		printf( "year %d of %d \n", y, AMOUNT_YEARS );
+		
+		for( int m = 0; m < AMOUNT_MOUNTHS; m++ ){
+
+			printf( "-> mounth %d of %d \n", m, AMOUNT_MOUNTHS );
+
+			for( int d = 0; d < AMOUNT_DAYS; d++ ){
+
+				printf( "--> day %d of %d \n", d, AMOUNT_DAYS );
+
+				printf( "Enter the day of the mounth \n" );
+				scanf( " %d", &index_day );
+				fflush( stdin );
+
+				printf( "Enter the amount of presents \n" );
+				scanf( " %d", &new_day[y][m][index_day].presents );
+				fflush( stdin );
+
+				printf( "Enter the amount of missings \n" );
+				scanf( " %d", &new_day[y][d][index_day].missings );
+				fflush( stdin );
+
+			}
+
+		}
+
+	}
+
+}
+
+void avg_presents( Days date_user[AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS] ){
+
+	int 	index_year,
+	    	index_mounth,
+	    	sum_presents = 0,
+	    	sum_studens = 0;
+	float 	avg;
+
+	printf( "Enter a year for calc the average \n" );
+	scanf( " %d", &index_year );
 	fflush( stdin );
 
-
-	for( int i = 0; i < MAX_GRADES; i++ ){
-
-		printf( "Enter the grade |%d-%d| \n", i+1, MAX_GRADES );
-		scanf( " %d", &new_student.all_grades[i] );
-		fflush( stdin );
-
-	}
-
-	printf( "Enter status of student \n" );
-	printf( "DESAPROBADO \t -> 0 \n" );
-	printf( "APROBADO PARCIAL -> 1 \n" );
-	printf( "APROBADO \t -> 2 \n" );
-
-	scanf( " %d", &new_student.final_grade );
+	printf( "Enter a mounth for calc the average \n" );
+	scanf( " %d", &index_mounth );
 	fflush( stdin );
+
+	for( int d = 0; d < AMOUNT_DAYS; d++ ){
+
+		sum_presents 	+= date_user[index_year][index_mounth][d].presents;
+		sum_studens 	+= ( date_user[index_year][index_mounth][d].missings + date_user[index_year][index_mounth][d].presents );
+
+	}
+
+	avg = sum_presents / ( AMOUNT_DAYS * 1.0 ); //correction by float divition
+
+	printf( "The average value for year %d, mounth %d, is %.2f \n", index_year, index_mounth, avg );
+
+}
+
+void max_presents( Days date_user[AMOUNT_YEARS][AMOUNT_MOUNTHS][AMOUNT_DAYS] ){
+
+	int amount_presents = 0,
+
 		
-	return new_student;
 
-}
 
-float calc_avg( Students calc_student ){
 
-	int sum_avg = 0;
-	float avg_grades;
-
-	for( int i = 0; i < MAX_GRADES; i++ ){
-
-		sum_avg += calc_student.all_grades[i];
-
-	}
-
-	avg_grades = sum_avg / ( MAX_GRADES * 1.0 );
-
-	return avg_grades;
-
-}
-
-float max_avg( Students student[MAX_FILES] ){
-
-	float m_avg = 0;
-
-	for( int i = 0; i < MAX_FILES; i++ ){
-
-		if( m_avg < student[i].average ){
-
-			m_avg = student[i].average;
-
-		}
-
-	}
-
-	return m_avg;
-
-}
-
-void print_results( Students files[MAX_FILES], float max_average ){
-
-	for( int i = 0; i < MAX_FILES; i++ ){
-
-		printf( "\n File %d \n", files[i].file );
-		
-		for( int j = 0; j < MAX_GRADES; j++ ){
-
-			printf( "-> grade[%d]: %d \n", j, files[i].all_grades[j] );
-
-		}
-
-		switch( files[i].final_grade ){
-
-			case DESAPROBADO:
-					printf( "Status Desaprobado \n" );
-					break;
-
-			case APROBADO_PRACIAL:
-					printf( "Status Aprobado parcial \n" );
-					break;
-
-			case APROBADO:
-					printf( "status Aprovado \n" );
-					break;
-
-		}
-
-		printf( "Average: %.2f \n", files[i].average );
-
-	}
-
-	printf( "Max average: %.2f \n", max_average );
-
-}
-	
